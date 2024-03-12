90e2c4691138c8d1b5163e841ef6d4e24c8bbe32
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2024-03-12 08:37:31 +0530

    an automated script added which create a battery limit service  on system startap

---
+++ SetupService.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Creating a Service file in /etc/systemd/system"
+
+sudo make install
+
+sudo touch /etc/systemd/system/BatteryLimitCharin.service
+while read -r line;
+do
+    echo $line>>/etc/systemd/system/BatteryLimitCharin.service
+
+done <template.txt
+echo "Automatic Generated Script "
+cat /etc/systemd/system/BatteryLimitCharin.service
+sudo systemctl enable BatteryLimitCharin.service 
+sudo systemctl start BatteryLimitCharin.service 
